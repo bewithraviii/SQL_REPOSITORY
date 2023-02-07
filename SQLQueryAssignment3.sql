b0b56a0fb6c74cb950004b5d13f54da5a6a1cfc1
unknown <tatva@PCA159.server7.com>
2023-02-07 15:29:21 +0530

    Completed Assignment 3

--- SQLQueryAssignment3.sql
+++ SQLQueryAssignment3.sql
@@ -1,16 +1,42 @@
+select * from Department
+select * from Employee
 
 
-create table Department
-(
-dept_id int primary key NOT NULL,
-dept_name nvarchar(40) NOT NULL
-)
 
-create table Employee
-(
-emp_id int primary key NOT NULL,
-dept_id int NOT NULL,
-mngr_id int NOT NULL,
-emp_name nvarchar(40) NOT NULL,
-salary int NOT NULL
-)
+
+select Department.dept_name, Employee.emp_name, Employee.salary
+from Employee
+join Department
+on Employee.dept_id = Department.dept_id
+where Employee.salary IN 
+	(
+		SELECT MAX(salary)
+		from Employee
+		group by dept_id
+	)
+order by Employee.salary DESC;
+
+
+select Department.dept_name as "Depertment Name"
+from Department
+inner join Employee 
+on Department.dept_id = Employee.dept_id
+Group by Department.dept_name
+Having COUNT(dept_name) < 3;
+
+
+
+select Department.dept_name, COUNT(Employee.emp_name) as "Employ No."
+from Department
+inner join Employee
+on Department.dept_id = Employee.dept_id
+Group by Department.dept_name
+
+
+
+
+select Department.dept_name, SUM(Employee.salary) as "Total Salary"
+from Department
+inner join Employee
+on Department.dept_id = Employee.dept_id
+Group by Department.dept_name
